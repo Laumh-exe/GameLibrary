a9e55f75d83038c143aa68b3f3ac6907148c7a5c
Laumh-exe <110572753+Laumh-exe@users.noreply.github.com>
2023-08-22 15:06:32 +0200

    1 add gamelibrary class (#10)
    * Added GameLibrary class
    
    games Arraylist is declared and initiated
    methods ignored for now
    
    * Inititializing textUI in constructor

---
+++ src/GameLibrary.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class GameLibrary {
+    private ArrayList<String> games;
+    private TextUI ui;
+    public GameLibrary() {
+        ArrayList<String> games = new ArrayList<>();
+        ui = new TextUI();
+    }
+}
