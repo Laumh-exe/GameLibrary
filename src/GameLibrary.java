8a8f6caa8891657bc28997db46235e31ef514a96
Laumh-exe <110572753+Laumh-exe@users.noreply.github.com>
2023-08-23 09:42:59 +0200

    3 displaymenu (#12)
    * Added GameLibrary class
    
    games Arraylist is declared and initiated
    methods ignored for now
    
    * Inititializing textUI in constructor
    
    * DisplayMenu() added
    
    * Added displayMenu(), updated gitignore
    
    DisplayMenu() will display many and ask user what they want to do.

--- src/GameLibrary.java
+++ src/GameLibrary.java
@@ -7,4 +7,26 @@ public class GameLibrary {
         ArrayList<String> games = new ArrayList<>();
         ui = new TextUI();
     }
+
+    public void displayMenu() {
+        while(true) {
+            int option = ui.getInput("1) Display Games\n2) Play Game\n3) Add Game\n4) Remove game");
+            switch (option) {
+                case 1:
+                    displayGameLibray();
+                    break;
+                case 2:
+                    playGame();
+                    break;
+                case 3:
+                    addGame();
+                    break;
+                case 4:
+                    removeGame();
+                    break;
+                default:
+                    ui.displayMessage("Please type a number to select an option from the list");
+            }
+        }
+    }
 }
