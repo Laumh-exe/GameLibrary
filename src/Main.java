73a1ae6d53772399a5f5268e0b8b9d7634ed3798
Laumh-exe <110572753+Laumh-exe@users.noreply.github.com>
2023-08-24 15:05:37 +0200

    Making it run (#22)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         GameLibrary gameLibrary = new GameLibrary();
+
+        gameLibrary.addGame("Baldurs Gate 3");
+        gameLibrary.addGame("Elden Ring");
+        gameLibrary.addGame("Shrek 2 The Video Game");
+        gameLibrary.addGame("Half Life 3");
+
+        gameLibrary.displayMenu();
     }
 }
